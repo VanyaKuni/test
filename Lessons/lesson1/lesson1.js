5bd82565c6604c31cc7ec89de7ee2de17acd6f5f
VanyaKuni <ivanterleckij8@gmail.com>
2025-08-13 01:17:57 +0300

    lesson2

---
+++ Lessons/lesson1/lesson1.js
@@ -0,0 +1,66 @@
+// let a = 6;
+// let b = 3;
+// if (a > b || a > 7) {
+//   console.log(a);
+// } else if (b > a) {
+//   console.log(b);
+//   console.log("b більше");
+// } else {
+//   console.log("вони одинакові");
+// }
+// я мавпа
+/////////////////////////////////////////////////////////////////////////
+// let mavpa = 6;
+// let begemot = 1;
+// let lev = 11;
+// if (mavpa > begemot && mavpa > lev) {
+//   console.log(mavpa);
+// } else if (begemot > mavpa && begemot > lev) {
+//   console.log(begemot);
+// } else {
+//   console.log(lev);
+// }
+// if (mavpa && begemot && lev > 10) {
+//   console.log("піздєц дохуя");
+// }
+/////////////////////////////////////////////////////////////////////////
+// let number = 52;
+// let res = number % 3;
+// console.log(res);
+/////////////////////////////////////////////////////////////////////////
+// if (5 === "5") {
+//   console.log("baran");
+// }
+// let ttt = "romb";
+// console.log(`ya mavpa ${ttt}`);
+/////////////////////////////////////////////////////////////////////////
+// let number = 6;
+// if (number % 2 == 0 && number % 3 == 0)
+//   console.log(`число ${number} ділеться на 2 і на 3`);
+// else if (number % 2 == 0) {
+//   console.log(`число ${number} ділиться на 2`);
+// } else if (number % 3 == 0) {
+//   console.log(`число ${number} ділиться на 3`);
+// } else {
+//   console.log("число не ділиться ні на 2 ні на 3");
+// }
+/////////////////////////////////////////////////////////////////////////
+// let maxdildo = 1;
+// let maxkuni = ++maxdildo;
+// console.log(maxkuni);
+// console.log(maxdildo);
+// let start = 1;
+// while (start < 10) {
+//   console.log("ya mavpa");
+//   start++;
+//   console.log(`zaraz start dorivhue ${start}`);
+// }
+/////////////////////////////////////////////////////////////////////////
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i++);
+// }
+/////////////////////////////////////////////////////////////////////////
